b487e97ba806fca7eb6dc11bba1964d2dc56884f
Dimitar Kolev <dimitar.kolev@novarto.com>
2017-11-02 14:30:19 +0200

    Fixed initial old swift code

--- TicTacToe/GameScene.swift
+++ TicTacToe/GameScene.swift
@@ -10,78 +10,68 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
+    var gameBoard: Board!
+    var stateMachine: GKStateMachine!
+    var ai: GKMinmaxStrategist!
     override func didMove(to view: SKView) {
+        /* Setup your scene here */
+        self.enumerateChildNodes(withName: "//grid*") { (node, stop) in
+            if let node = node as? SKSpriteNode{
+                node.color = UIColor.clear
+            }
+        }
+        let top_left: BoardCell  = BoardCell(value: .None, node: "//*top_left")
+        let top_middle: BoardCell = BoardCell(value: .None, node: "//*top_middle")
+        let top_right: BoardCell = BoardCell(value: .None, node: "//*top_right")
+        let middle_left: BoardCell = BoardCell(value: .None, node: "//*middle_left")
+        let center: BoardCell = BoardCell(value: .None, node: "//*center")
+        let middle_right: BoardCell = BoardCell(value: .None, node: "//*middle_right")
+        let bottom_left: BoardCell = BoardCell(value: .None, node: "//*bottom_left")
+        let bottom_middle: BoardCell = BoardCell(value: .None, node: "//*bottom_middle")
+        let bottom_right: BoardCell = BoardCell(value: .None, node: "//*bottom_right")
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        let board = [top_left, top_middle, top_right, middle_left, center, middle_right, bottom_left, bottom_middle, bottom_right]
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        gameBoard = Board(gameboard: board)
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        for touch in touches {
+            let location = touch.location(in: self)
+            let selectedNode = self.atPoint(location)
+            var node: SKSpriteNode
+            
+            if let name = selectedNode.name {
+                if name == "Reset" || name == "reset_label"{
+                    self.stateMachine.enter(StartGameState.self)
+                    return
+                }
+            }
+            
+            if gameBoard.isPlayerOne(){
+                let cross = SKSpriteNode(imageNamed: "X_symbol")
+                cross.size = CGSize(width: 75, height: 75)
+                cross.zRotation = CGFloat(Double.pi / 4.0)
+                node = cross
+            }
+            else{
+                let circle = SKSpriteNode(imageNamed: "O_symbol")
+                circle.size = CGSize(width: 75, height: 75)
+                node = circle
+            }
+            
+            for i in 0...8{
+                guard let cellNode: SKSpriteNode = self.childNode(withName: gameBoard.getElementAtBoardLocation(index: i).node) as? SKSpriteNode else{
+                    return
+                }
+                if selectedNode.name == cellNode.name{
+                    cellNode.addChild(node)
+                    gameBoard.addPlayerValueAtBoardLocation(index: i, value: gameBoard.isPlayerOne() ? .X : .O)
+                    gameBoard.togglePlayer()
+                }
+            }
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
--- TicTacToe/GameViewController.swift
+++ TicTacToe/GameViewController.swift
@@ -15,20 +15,19 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
+        if let scene = GameScene(fileNamed:"GameScene") {
+            // Configure the view.
+            let skView = self.view as! SKView
+            skView.showsFPS = true
+            skView.showsNodeCount = true
             
-            view.ignoresSiblingOrder = true
+            /* Sprite Kit applies additional optimizations to improve rendering performance */
+            skView.ignoresSiblingOrder = true
             
-            view.showsFPS = true
-            view.showsNodeCount = true
+            /* Set the scale mode to scale to fit the window */
+            scene.scaleMode = .aspectFill
+            
+            skView.presentScene(scene)
         }
     }
 
